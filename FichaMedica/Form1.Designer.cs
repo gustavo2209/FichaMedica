009a31fc0fb0366a8e1c55b39acf3f23f433f390
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-14 10:23:01 -0500

    Agregar archivos de proyecto.

---
+++ FichaMedica/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace FichaMedica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbMasculino = new System.Windows.Forms.RadioButton();
+            this.rdbFemenino = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkNo = new System.Windows.Forms.RadioButton();
+            this.chkSi = new System.Windows.Forms.RadioButton();
+            this.txtseguro = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkPartos = new System.Windows.Forms.CheckBox();
+            this.chkProstata = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPaciente);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(332, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paciente";
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.Location = new System.Drawing.Point(19, 28);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(294, 20);
+            this.txtPaciente.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbFemenino);
+            this.groupBox2.Controls.Add(this.rdbMasculino);
+            this.groupBox2.Location = new System.Drawing.Point(12, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(153, 87);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sexo";
+            // 
+            // rdbMasculino
+            // 
+            this.rdbMasculino.AutoSize = true;
+            this.rdbMasculino.Location = new System.Drawing.Point(19, 28);
+            this.rdbMasculino.Name = "rdbMasculino";
+            this.rdbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rdbMasculino.TabIndex = 0;
+            this.rdbMasculino.TabStop = true;
+            this.rdbMasculino.Text = "Masculino";
+            this.rdbMasculino.UseVisualStyleBackColor = true;
+            this.rdbMasculino.CheckedChanged += new System.EventHandler(this.rdbMasculino_CheckedChanged);
+            // 
+            // rdbFemenino
+            // 
+            this.rdbFemenino.AutoSize = true;
+            this.rdbFemenino.Location = new System.Drawing.Point(19, 51);
+            this.rdbFemenino.Name = "rdbFemenino";
+            this.rdbFemenino.Size = new System.Drawing.Size(71, 17);
+            this.rdbFemenino.TabIndex = 2;
+            this.rdbFemenino.TabStop = true;
+            this.rdbFemenino.Text = "Femenino";
+            this.rdbFemenino.UseVisualStyleBackColor = true;
+            this.rdbFemenino.CheckedChanged += new System.EventHandler(this.rdbMasculino_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtseguro);
+            this.groupBox3.Controls.Add(this.chkNo);
+            this.groupBox3.Controls.Add(this.chkSi);
+            this.groupBox3.Location = new System.Drawing.Point(12, 176);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(153, 87);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Seguro";
+            // 
+            // chkNo
+            // 
+            this.chkNo.AutoSize = true;
+            this.chkNo.Location = new System.Drawing.Point(19, 51);
+            this.chkNo.Name = "chkNo";
+            this.chkNo.Size = new System.Drawing.Size(39, 17);
+            this.chkNo.TabIndex = 2;
+            this.chkNo.TabStop = true;
+            this.chkNo.Text = "No";
+            this.chkNo.UseVisualStyleBackColor = true;
+            this.chkNo.CheckedChanged += new System.EventHandler(this.chkSi_CheckedChanged);
+            // 
+            // chkSi
+            // 
+            this.chkSi.AutoSize = true;
+            this.chkSi.Location = new System.Drawing.Point(19, 28);
+            this.chkSi.Name = "chkSi";
+            this.chkSi.Size = new System.Drawing.Size(34, 17);
+            this.chkSi.TabIndex = 0;
+            this.chkSi.TabStop = true;
+            this.chkSi.Text = "Si";
+            this.chkSi.UseVisualStyleBackColor = true;
+            this.chkSi.CheckedChanged += new System.EventHandler(this.chkSi_CheckedChanged);
+            // 
+            // txtseguro
+            // 
+            this.txtseguro.Location = new System.Drawing.Point(73, 25);
+            this.txtseguro.Name = "txtseguro";
+            this.txtseguro.Size = new System.Drawing.Size(60, 20);
+            this.txtseguro.TabIndex = 3;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.checkBox5);
+            this.groupBox4.Controls.Add(this.checkBox4);
+            this.groupBox4.Controls.Add(this.chkProstata);
+            this.groupBox4.Controls.Add(this.chkPartos);
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Location = new System.Drawing.Point(191, 83);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(153, 180);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Historia Clinica";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(22, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Sarampión";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // chkPartos
+            // 
+            this.chkPartos.AutoSize = true;
+            this.chkPartos.Location = new System.Drawing.Point(22, 57);
+            this.chkPartos.Name = "chkPartos";
+            this.chkPartos.Size = new System.Drawing.Size(56, 17);
+            this.chkPartos.TabIndex = 4;
+            this.chkPartos.Text = "Partos";
+            this.chkPartos.UseVisualStyleBackColor = true;
+            // 
+            // chkProstata
+            // 
+            this.chkProstata.AutoSize = true;
+            this.chkProstata.Location = new System.Drawing.Point(22, 86);
+            this.chkProstata.Name = "chkProstata";
+            this.chkProstata.Size = new System.Drawing.Size(65, 17);
+            this.chkProstata.TabIndex = 4;
+            this.chkProstata.Text = "Próstata";
+            this.chkProstata.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(22, 115);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(71, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Obesidad";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(22, 144);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(64, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Tensión";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(357, 277);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ficha Médica";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbFemenino;
+        private System.Windows.Forms.RadioButton rdbMasculino;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtseguro;
+        private System.Windows.Forms.RadioButton chkNo;
+        private System.Windows.Forms.RadioButton chkSi;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox chkProstata;
+        private System.Windows.Forms.CheckBox chkPartos;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ FichaMedica/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FichaMedica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdbMasculino.Checked = true;
+            chkSi.Checked = true;
+            chkPartos.Enabled = false;
+            txtPaciente.Focus();
+        }
+
+        private void rdbMasculino_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbMasculino.Checked)
+            {
+                chkProstata.Enabled = true;
+                chkPartos.Enabled = false;
+                chkPartos.Checked = false;
+            }
+            if (rdbFemenino.Checked)
+            {
+                chkProstata.Enabled = false;
+                chkPartos.Enabled = true;
+                chkProstata.Checked = false;
+            }
+        }
+
+        private void chkSi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkNo.Checked)
+            {
+                txtseguro.Visible = false;
+            }
+            if(chkSi.Checked){
+                txtseguro.Visible = true;
+                txtseguro.Focus();
+            }
+        }
+    }
+}
